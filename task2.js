f58fd0b98a90470a93926958e4d21e4566d2fb81
Alibek <Alibek.zh09@gmail.com>
2024-07-09 20:45:01 +0200

    JS code is added successfully

---
+++ task2.js
@@ -0,0 +1,12 @@
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    console.log('Thank you for your message!');
+});
+
+function playMusic() {
+    console.log('Music is playing!');
+}
+
+function pauseMusic() {
+    console.log('Music is paused!');
+}
